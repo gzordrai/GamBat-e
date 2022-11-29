bbe9c4afbf9b54504aea1b6280d58a04add8e4a3
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-11-29 22:30:44 +0100

    Rework main file after bot core update

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,39 @@
-import { Client, ClientOptions, GatewayIntentBits, Interaction, Message, Partials, VoiceState } from "discord.js";
+import { GatewayIntentBits } from "discord.js";
 import { config } from "dotenv";
+import { readdirSync } from "fs";
 import path from "path";
-import { ExtendedClient } from "./bot";
-import { interactionCreate, messageCreate, ready, voiceStateUpdate } from "./listeners";
+import { ExtendedClient, Command, Event } from "./bot";
 
-config({ path: path.resolve(__dirname, "../.env") });
+config({ path: "../.env" });
 
-const options: ClientOptions = {
+const client: ExtendedClient = new ExtendedClient({
     intents: [
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildVoiceStates
+        GatewayIntentBits.Guilds
     ],
     partials: [
-        Partials.Channel,
-        Partials.Message
+
     ]
-};
-const client: ExtendedClient = new ExtendedClient(options);
+});
+const commandsPath: string = path.join(__dirname, "commands");
+const eventsPath: string = path.join(__dirname, "events");
+const commandFiles: Array<string> = readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+const eventFiles: Array<string> = readdirSync(eventsPath).filter(file => file.endsWith(".js"));
 
-console.log("Bot is starting...");
+for (const file of commandFiles) {
+    const filePath: string = path.join(commandsPath, file);
+    const command: Command = require(filePath).default;
 
-client.on("interactionCreate", async (interaction: Interaction) => await interactionCreate(client, interaction));
-client.on("messageCreate", async (message: Message) => await messageCreate(message));
-client.once("ready", async (client: Client) => await ready(client));
-client.on("voiceStateUpdate", async (oldState: VoiceState, newState: VoiceState) => await voiceStateUpdate(oldState, newState));
+    client.commands.set(command.data.name, command);
+}
 
-client.login(process.env.TOKEN!);
+for (const file of eventFiles) {
+    const filePath: string = path.join(eventsPath, file);
+    const event: Event = require(filePath).default;
+
+    if (event.once)
+        client.once(event.name, (...args) => event.execute(client, ...args));
+    else
+        client.on(event.name, (...args) => event.execute(client, ...args));
+}
+
+client.login(process.env.TOKEN);
