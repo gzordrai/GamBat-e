9020a3c6127e676c41eea5055e0e62d5f7b6d57e
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-07-12 23:36:07 +0200

    Added a collection for predictions

--- src/bot/ExtendedClient.ts
+++ src/bot/ExtendedClient.ts
@@ -1,6 +1,8 @@
 import { Client, Collection } from "discord.js";
 import { Command } from "./Command";
+import { Prediction } from "../prediction";
 
 export class ExtendedClient extends Client {
     public commands: Collection<string, Command> = new Collection<string, Command>();
+    public predictions: Collection<string, Prediction> = new Collection<string, Prediction>();
 }
