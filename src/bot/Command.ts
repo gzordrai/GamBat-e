89751892bf324cd9d3f6b71554834ed3b0b0a660
gzordrai <gzordrai@users.noreply.github.com>
2023-10-18 11:11:34 +0200

    Improve bot structure and add new features

--- src/bot/Command.ts
+++ src/bot/Command.ts
@@ -1,8 +1,14 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js";
+import { AutocompleteInteraction, ChatInputCommandInteraction, Collection, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js";
 import { ExtendedClient } from "./ExtendedClient";
 
 export interface Command {
     data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup"> | SlashCommandSubcommandsOnlyBuilder;
     modal: boolean;
+    subcommands?: Collection<string, Subcommand>;
+    autocomplete?: (interaction: AutocompleteInteraction, client?: ExtendedClient) => Promise<void>;
     execute: (interaction: ChatInputCommandInteraction<"cached">, client: ExtendedClient | undefined) => Promise<void>;
+}
+
+export interface Subcommand {
+    modal: boolean;
 }
