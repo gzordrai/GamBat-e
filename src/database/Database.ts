e8080a92d734f7253b84326d5f4373bc922da60d
gzordrai <gzordrai@users.noreply.github.com>
2023-10-19 22:55:09 +0200

    Implement database connection

--- src/database/Database.ts
+++
@@ -1,6 +0,0 @@
-import { Config, JsonDB } from "node-json-db";
-import path from "path";
-
-export class Database {
-    protected database: JsonDB = new JsonDB(new Config(path.resolve(__dirname, "../../data/database.json"), true, false, '/'));
-}
---
+++ src/database/database.ts
@@ -0,0 +1,15 @@
+import { config } from "dotenv";
+import { createConnection } from "mongoose";
+import { join } from "path";
+
+config({ path: join(__dirname, "../../.env") });
+
+const MONGODB_URI: string = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`;
+
+const connection = createConnection(MONGODB_URI);
+
+connection.on("connected", () => console.log(`Connected to ${MONGODB_URI}`));
+connection.on("disconnected", () => console.log(`Disconnected to ${MONGODB_URI}`));
+connection.on("error", () => {});
+
+export default connection;
