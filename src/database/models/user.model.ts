69f19ce272a2dd359560ef7fff73c46f6aa6243c
gzordrai <gzordrai@users.noreply.github.com>
2023-10-20 19:55:52 +0200

    Add methods and static method to User model

--- src/database/models/user.model.ts
+++ src/database/models/user.model.ts
@@ -1,14 +1,41 @@
-import { Model, Schema, model } from "mongoose";
+import { Model, Schema } from "mongoose";
+import connection from "../database";
 
 interface IUser {
     id: string;
     balance: number;
 }
 
-const UserSchema: Schema<IUser> = new Schema<IUser>({
+interface IUserMethods {
+    addToBalance: (amount: number) => Promise<void>;
+    subsFromBalance: (amount: number) => Promise<void>;
+}
+
+interface IUserModel extends Model<IUser> {
+    findOneOrCreate: (condition: any, schema: any) => Promise<any>;
+}
+
+const UserSchema = new Schema<IUser, IUserModel>({
     id: { type: String, required: true },
     balance: { type: Number, required: true, default: 0 }
+})
+
+UserSchema.methods.addToBalance = async function (amount: number): Promise<void> {
+    this.balance += amount;
+
+    await this.save();
+}
+
+UserSchema.methods.subsFromBalance = async function (amount: number): Promise<void> {
+    this.balance -= amount;
+
+    await this.save();
+}
+
+UserSchema.static("findOneOrCreate", async function (condition, schema) {
+    const user = await User.findOne(condition);
+
+    return user || await User.create(schema);
 });
 
-
-export const User: Model<IUser> = model<IUser>("User", UserSchema);
+export const User: IUserModel = connection.model<IUser, IUserModel>("User", UserSchema);
