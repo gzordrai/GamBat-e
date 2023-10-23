6fb38eadd0a115ac72a30c28f8ecd6cc183031bb
gzordrai <gzordrai@users.noreply.github.com>
2023-10-24 00:54:36 +0200

    Update debug logs for Github action

--- src/database/database.ts
+++ src/database/database.ts
@@ -4,12 +4,12 @@ import { join } from "path";
 
 config({ path: join(__dirname, process.env.NODE_ENV === "test" ? "../../.env.test" : "../../.env") });
 
-console.log(process.env.NODE_ENV);
-
 const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
 const MONGODB_URI: string = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authMechanism=DEFAULT`;
 
 const connection: Connection = createConnection(MONGODB_URI);
+
+console.log(MONGODB_URI);
 
 connection.on("connected", () => console.log(`Connected to database`));
 connection.on("disconnected", () => console.log(`Disconnected to database`));
