fa248f03db4dd466af5525b460f32fef166c50b6
gzordrai <gzordrai@users.noreply.github.com>
2023-10-28 18:42:13 +0200

    Add choice id to interactions custom id

--- src/handlers/modal.ts
+++ src/handlers/modal.ts
@@ -17,16 +17,16 @@ const handlePredictionModal = async (interaction: ModalSubmitInteraction<"cached
     const timer: number = parseInt(interaction.customId.split('-')[1])
     const predection: Prediction = new Prediction(timer);
     const row: ActionRowBuilder<ButtonBuilder> = new ActionRowBuilder<ButtonBuilder>();
-    let i: number = 1;
+    let i: number = 0;
 
     interaction.fields.fields.forEach((field) => {
         if (field.value !== "") {
             if (field.customId.startsWith("choice")) {
-                predection.addChoice({ name: field.value, oods: 1 });
+                predection.addChoice({ id: i, name: field.value, oods: 1 });
 
                 row.addComponents(
                     new ButtonBuilder()
-                        .setCustomId(`choice-${i - 1}`)
+                        .setCustomId(`choice-${i}`)
                         .setLabel(field.value.substring(0, 80))
                         .setStyle(ButtonStyle.Primary)
                 );
@@ -53,4 +53,12 @@ const handleBetModal = async (interaction: ModalSubmitInteraction<"cached">, cli
         interaction.followUp({ content: "Le montant du pari doit être un nombre entier strictement supérieur à 1.", ephemeral: true });
         return;
     }
+
+    console.log(interaction.customId.split('-'));
+
+    prediction.addBet({
+        userId: interaction.user.id,
+        choice: parseInt(interaction.customId.split('-')[2]),
+        amount: bet
+    });
 }
